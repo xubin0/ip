d04c050056ed58d7f255c08ac02823f7b1f7f48c
xb <>
2025-02-03 11:57:24 +0800

    Add function to store text input
    User input will be stored in list, list can be displayed with "list" input.

--- src/main/java/xb.java
+++ src/main/java/xb.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
+
 public class xb {
     public static void main(String[] args) {
+        String[] list = new String[100];
         Scanner s = new Scanner(System.in);
         System.out.println("Hello, I am xb!");
         System.out.println("What is your name?");
         String name = s.nextLine();
         System.out.print(name);
-        System.out.println(", what can I do for you?(type bye to exit)");
+        System.out.println(", what can I do for you?(\"bye\" to exit, \"list\" to show list)");
         String echo;
-        do{
+        int itemCount = 0;
+        while (true) {
             echo = s.nextLine();
-            System.out.println(echo);
-        }while(!(echo.equals("bye")));
+            if (echo.equals("bye")) {
+                break;
+            }
+            if (echo.equals("list")) {
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                continue;
+            }
+            System.out.println("added: " + echo);
+            list[itemCount] = echo;
+            itemCount++;
+        }
 
-        System.out.println(echo);
         System.out.println("Bye. Hope to see you again soon!");
         s.close();
-
     }
 }
