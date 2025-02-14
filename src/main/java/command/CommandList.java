e38724629c489b37bb767c040210d77a1685b632
xb <>
2025-02-14 12:16:31 +0800

    Add XbException class with exceptions. Modify CommandList Enum to handle possible exceptions.

--- src/main/java/command/CommandList.java
+++ src/main/java/command/CommandList.java
@@ -1,31 +1,51 @@
 package command;
 
+import exception.XbException;
 import task.*;
 import util.IntegerChecker;
+
 
 public enum CommandList {
     MARK {
         @Override
         public void execute(String[] parts, TaskList taskList) {
-            if (parts.length == 2 && IntegerChecker.isInteger(parts[1])) {
-                taskList.markTaskAsDone(Integer.parseInt(parts[1]));
+            try {
+                if (parts.length == 2 && IntegerChecker.isInteger(parts[1])) {
+                    taskList.markTaskAsDone(Integer.parseInt(parts[1]));
+                } else {
+                    throw XbException.invalidMarkCommand();
+                }
+            } catch (XbException e) {
+                System.out.println(e.getMessage());
             }
         }
     },
     UNMARK {
         @Override
         public void execute(String[] parts, TaskList taskList) {
-            if (parts.length == 2 && IntegerChecker.isInteger(parts[1])) {
-                taskList.markTaskAsUndone(Integer.parseInt(parts[1]));
+            try {
+                if (parts.length == 2 && IntegerChecker.isInteger(parts[1])) {
+                    taskList.markTaskAsUndone(Integer.parseInt(parts[1]));
+                } else {
+                    throw XbException.invalidUnmarkCommand();
+                }
+            } catch (XbException e) {
+                System.out.println(e.getMessage());
             }
         }
     },
     TODO {
         @Override
         public void execute(String[] parts, TaskList taskList) {
-            if (parts.length == 2) {
-                Todo todo = new Todo(taskList.getTaskCount() + 1, parts[1], false);
-                taskList.addTask(todo);
+            try {
+                if (parts.length == 2) {
+                    Todo todo = new Todo(taskList.getTaskCount() + 1, parts[1], false);
+                    taskList.addTask(todo);
+                } else {
+                    throw XbException.invalidTodoCommand();
+                }
+            }catch (XbException e){
+                System.out.println(e.getMessage());
             }
 
         }
@@ -33,25 +53,38 @@ public enum CommandList {
     DEADLINE {
         @Override
         public void execute(String[] parts, TaskList taskList) {
-            if (parts.length == 2 && parts[1].contains("/by")) {
-                String[] deadlineParts = parts[1].split("/by", 2);
-                String taskName = deadlineParts[0].trim();
-                String deadline = deadlineParts[1].trim();
-                Deadline dl = new Deadline(taskList.getTaskCount() + 1, taskName, false, deadline);
-                taskList.addTask(dl);
+            try {
+                if (parts.length == 2 && parts[1].contains("/by")) {
+                    String[] deadlineParts = parts[1].split("/by", 2);
+                    String taskName = deadlineParts[0].trim();
+                    String deadline = deadlineParts[1].trim();
+                    Deadline dl = new Deadline(taskList.getTaskCount() + 1, taskName, false, deadline);
+                    taskList.addTask(dl);
+                } else {
+                    throw XbException.invalidDeadlineCommand();
+                }
+            } catch (XbException e) {
+                System.out.println(e.getMessage());
             }
+
         }
     },
     EVENT {
         @Override
         public void execute(String[] parts, TaskList taskList) {
-            if (parts.length == 2 && parts[1].contains("/from") && parts[1].contains("/to")) {
-                String[] eventParts = parts[1].split("/from|/to");
-                String taskName = eventParts[0].trim();
-                String from = eventParts[1].trim();
-                String to = eventParts[2].trim();
-                Event event = new Event(taskList.getTaskCount() + 1, taskName, false, from, to);
-                taskList.addTask(event);
+            try {
+                if (parts.length == 2 && parts[1].contains("/from") && parts[1].contains("/to")) {
+                    String[] eventParts = parts[1].split("/from|/to");
+                    String taskName = eventParts[0].trim();
+                    String from = eventParts[1].trim();
+                    String to = eventParts[2].trim();
+                    Event event = new Event(taskList.getTaskCount() + 1, taskName, false, from, to);
+                    taskList.addTask(event);
+                } else {
+                    throw XbException.invalidEventCommand();
+                }
+            } catch (XbException e) {
+                System.out.println(e.getMessage());
             }
         }
     },
@@ -59,7 +92,7 @@ public enum CommandList {
     LIST {
         @Override
         public void execute(String[] parts, TaskList taskList) {
-            if(taskList.getTaskCount() > 0) {
+            if (taskList.getTaskCount() > 0) {
                 taskList.printList();
                 return;
             }
---
+++ src/main/java/exception/XbException.java
@@ -0,0 +1,27 @@
+package exception;
+
+public class XbException extends Exception {
+    public XbException(String message) {
+        super(message);
+    }
+
+    public static XbException invalidMarkCommand() {
+        return new XbException("Invalid mark command, please specify which task to mark with an integer.");
+    }
+
+    public static XbException invalidUnmarkCommand() {
+        return new XbException("Invalid unmark command, please specify which task to unmark with an integer.");
+    }
+
+    public static XbException invalidDeadlineCommand() {
+        return new XbException("Invalid deadline command, please specify when deadline is in the future.");
+    }
+
+    public static XbException invalidEventCommand() {
+        return new XbException("Invalid event command, please specify duration of event with \"from\" and \"to\".");
+    }
+    public static XbException invalidTodoCommand() {
+        return new XbException("Invalid Todo command, please name of Task to be done.");
+    }
+
+}
